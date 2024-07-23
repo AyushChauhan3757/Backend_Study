c4472bd965187fe4b31ca955bf50590cd8e364fc
Ayush Chauhan <65812075+AyushChauhan3757@users.noreply.github.com>
2024-07-23 18:15:58 +0000

    Setup PHP and MYSQL

--- PHP_01/index.php
+++ PHP_01/index.php
@@ -1,2 +1,12 @@
 <?php
-    echo "Hello World";
+    
+    // 1. Create a database connection
+    $db = mysqli_connect("127.0.0.1", "mariadb", "mariadb", "mariadb", 3306);
+
+    // Test if connection succeeded (recommended)
+    if(mysqli_connect_errno()) {
+        echo "Error";
+    }
+    else {
+        echo "Connected";
+    }
