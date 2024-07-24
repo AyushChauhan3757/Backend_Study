8ffb3ec22a67b3f616d053dcd8cad5d13a347224
Ayush Chauhan <65812075+AyushChauhan3757@users.noreply.github.com>
2024-07-24 15:36:29 +0000

    Connecting to a Database

--- PHP_01/index.php
+++ PHP_01/index.php
@@ -1,6 +1,9 @@
 <?php
-    
-    // 1. Create a database connection
+    /**
+    * This script demonstrates how to create a database connection using mysqli and PDO in PHP.
+    */
+
+    // 1. Create a database connection using mysqli
     $db = mysqli_connect("127.0.0.1", "mariadb", "mariadb", "mariadb", 3306);
 
     // Test if connection succeeded (recommended)
@@ -9,4 +12,16 @@
     }
     else {
         echo "Connected";
+    }
+
+    // 2. Create a database connection using PDO
+    $dsn = "mysql:host=127.0.0.1;dbname=mariadb";
+    $dbusername = "mariadb";
+    $dbpassword = "mariadb";
+
+    try {
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        echo "Connected";
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
